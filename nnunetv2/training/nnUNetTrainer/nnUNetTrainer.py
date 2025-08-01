f0d77ab56f41dfd64f15bae16ed361624da46192
Crower-1 <51692171+Crower-1@users.noreply.github.com>
2025-08-01 15:15:10 +0800

    Fix init kwargs storage for finetune trainer

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -108,7 +108,7 @@ class nnUNetTrainer(object):
         # would also pickle the network etc. Bad, bad. Instead we just reinstantiate and then load the checkpoint we
         # need. So let's save the init args
         self.my_init_kwargs = {}
-        for k in inspect.signature(self.__init__).parameters.keys():
+        for k in inspect.signature(nnUNetTrainer.__init__).parameters.keys():
             self.my_init_kwargs[k] = locals()[k]
 
         ###  Saving all the init args into class variables for later access
